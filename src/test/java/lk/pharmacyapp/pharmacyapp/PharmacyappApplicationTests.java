ac2e1cf211f7650a51a6b880e45f57f482071b9b
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-03-08 12:15:14 +0530

    first commit

---
+++ src/test/java/lk/pharmacyapp/pharmacyapp/PharmacyappApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.pharmacyapp.pharmacyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
